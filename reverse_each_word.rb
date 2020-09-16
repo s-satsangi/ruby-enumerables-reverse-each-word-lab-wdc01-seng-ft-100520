ab441bce1d157c30652cac725b450235bc45726f
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-16 03:57:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word sentence 
   array = sentence.split(" ")
   sentence = ""
-  array.each{|word| sentence += word.reverse }
+  array.each{|word| sentence += word.reverse + " " }
   
   sentence
 end
