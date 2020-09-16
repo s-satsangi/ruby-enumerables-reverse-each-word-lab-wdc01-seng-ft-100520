444b7e40a3049b7b6439a77bbaa1769ccf39fe52
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-16 03:52:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def reverse_each_word sentence 
   array = sentence.split(" ")
-  array.each{|word| word=word.reverse}
-  binding.pry
+  array.each{|word| word=word.reverse  binding.pry}
+  
   sentence=array.join(" ")
 end
