5d716b757ea87d75aed59505f39d14ef156a2071
Sharad Satsangi <s.satsangi@gmail.com>
2020-09-16 04:08:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,14 @@
 require 'pry'
 def reverse_each_word sentence 
-  array = sentence.split(" ")
-  sentence = ""
-  array.each{|word| sentence += word.reverse + " " }
-  
-  sentence.slice(0, sentence.length-1)
+
+#First, solved with each
+#  array = sentence.split(" ")
+#  sentence = ""
+#  array.each{|word| sentence += word.reverse + " " }
+#  
+#  sentence.slice(0, sentence.length-1)
+
+#second, using collect 
+
+
 end
